89cd8ff00b6126fb2f2dfd871c90b4126ca5d835
KevinLindemark <reclaimyourdata@tutamail.com>
2023-12-05 23:58:07 +0100

    formatted and added docstrings

--- lib/lcd_menu.py
+++ lib/lcd_menu.py
@@ -1,21 +1,22 @@
 import machine
-import utime
 import uasyncio as asyncio
+
 
 class LCDMenu:
     def __init__(self, lcd, encoder, button):
+        """Constructor, takes LCD, encoder and button pin object as arguments"""
         self.lcd = lcd
         self.encoder = encoder
         self.button = button
-        
+
         self.menu_items = []  # A list to store menu items
         self.line = 1  # Index of the current menu
         self.highlight = 1  # Index of the selected item in the current menu
         self.shift = 0
         self.list_length = 0
         self.total_lines = 4
-        self.selected_item = 0
-         
+        self.selected_item = 0        
+        
         self.encoder_last_state = None  # Store the last state of the encoder
         self.encoder_button_pressed = False  # Flag to track button press
 
@@ -24,71 +25,68 @@ class LCDMenu:
         self.initialize_button()
 
     def button_callback(self, pin):
-        # Callback function for button press
+        """Callback function for button press"""
         if self.encoder_button_pressed == False:
             self.encoder_button_pressed = True
 
     def initialize_lcd(self):
-        # Initialize and clear the LCD display
+        """Initialize and clear the LCD display"""
         self.lcd.clear()
         self.lcd.move_to(0, 0)
 
     def initialize_encoder(self):
-        # Initialize the rotary encoder
+        """Initialize the rotary encoder"""
         self.encoder_last_state = self.encoder.re_full_step()
 
     def initialize_button(self):
-        # Initialize the button
+        """Initialize the button"""
         self.button.irq(handler=self.button_callback, trigger=machine.Pin.IRQ_FALLING)
 
     def add_menu_item(self, text, callback=None):
-        # Add a menu item to the current menu
+        """Add a menu item to the current menu"""
         self.menu_items.append({"text": text, "callback": callback})
 
     def display_menu(self):
-        # Display the current menu
-        print(f"shift {self.highlight}")
+        """Display the current menu"""
         self.lcd.clear()
         self.item = 1
         self.line = 1
-        self.list_length = len(self.menu_items) # self.shift the list of files so that it shows on the display
+        self.list_length = len(self.menu_items) #  the list of files so that it shows on the display
+        self.shift 
         short_list = self.menu_items[self.shift:self.shift+self.total_lines]
                 
         for item in short_list:
-            #print(item)
             if self.highlight == self.line:
                 self.lcd.move_to(1, self.line-1)
                 self.lcd.putstr(item["text"])
                 self.selected = self.line-1
-                
             else:
-                
                 self.lcd.move_to(1, self.line-1)
                 self.lcd.putstr(item["text"])
-            
             self.line += 1
             self.lcd.move_to(0, self.selected)
             self.lcd.blink_cursor_on()
-    
+
     def navigate_menu(self):
+        """Navigate menu system"""
         while True:
             # Read the current state of the encoder
             res = self.encoder.re_full_step()
-            if res == -1:  # Turned Left 
+            if res == -1:  # Turned Left
                 if self.highlight > 1:
-                    self.highlight -= 1  
+                    self.highlight -= 1
                 else:
                     if self.shift > 0:
                         self.shift -= 1
                 self.display_menu()
-                #show_menu(file_list)
-                
+                # show_menu(file_list)
+
             if res == 1:    # Turned Right
                 if self.highlight < self.total_lines:
                     self.highlight += 1
-                else: 
+                else:
                     if self.shift+self.total_lines < self.list_length:
-                       self.shift += 1
+                        self.shift += 1
 
                 self.display_menu()
 
@@ -97,17 +95,20 @@ class LCDMenu:
 
             # Check if the button is pressed
             if self.encoder_button_pressed:
-                callback = self.menu_items[self.selected+self.shift]["callback"] # add shift to selected to get correct callback
+
+                #  add shift to selected to get correct callback
+                callback = self.menu_items[self.selected+self.shift]["callback"]
                 if callback:
                     callback()
 
             if self.encoder_button_pressed == True:
-                await asyncio.sleep_ms(200)  # Adjust the debounce delay as needed
+                #  add shift to selected to get correct callback
+                await asyncio.sleep_ms(200)
                 # Reset the button state
                 self.encoder_button_pressed = False
-            
+
     def run(self):
-        # Start the navigation loop
+        """Start the navigation loop"""
         loop = asyncio.get_event_loop()
         loop.create_task(self.navigate_menu())
         loop.run_forever()
